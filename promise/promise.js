9e1cb5c65c2af2097304de042bd91c65893b5f9b
minh hoang <21ducminh@gmail.com>
2024-01-04 17:28:12 +0700

    Promise day 2

--- promise/promise.js
+++ promise/promise.js
@@ -1,19 +1,35 @@
-function sleep(ms) {
-    return new Promise((resolve) => {
-        setTimeout(resolve, 1000);
-    });
-}
+// function sleep(ms) {
+//     return new Promise((resolve) => {
+//         setTimeout(resolve, 1000);
+//     });
+// }
 
-sleep(1000)
-    .then(() => {
-        console.log(1);
-        return sleep(1000);
+// sleep(1000)
+//     .then(() => {
+//         console.log(1);
+//         return sleep(1000);
+//     })
+//     .then(() => {
+//         console.log(2);
+//         return sleep(1000);
+//     })
+//     .then(() => {
+//         console.log(3);
+//         return sleep(1000);
+//     })
+
+var promise1 = new Promise((resolve) => {
+    setTimeout(() => resolve([1]), 1000);
+});
+
+var promise2 = new Promise((resolve) => {
+    setTimeout(() => resolve([2, 3]), 2000);
+});
+
+Promise.all([promise1, promise2])
+    .then(([result1, result2]) => {
+        console.log(result1.concat(result2));
     })
-    .then(() => {
-        console.log(2);
-        return sleep(1000);
-    })
-    .then(() => {
-        console.log(3);
-        return sleep(1000);
+    .catch(() => {
+        console.log("error");
     })
